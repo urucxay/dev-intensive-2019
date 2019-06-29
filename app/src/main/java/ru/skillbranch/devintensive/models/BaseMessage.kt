93d640b3194b75f795d1ce512e0ffed5a69cc88a
UrucXay <UrucXay@gmail.com>
2019-06-29 10:35:18 +0300

    BaseMessage was corrected. in fun makeMessage was added isIncoming

--- app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
@@ -12,11 +12,11 @@ abstract class BaseMessage(
     abstract fun formatMessage(): String
     companion object AbstractFactory{
         var lastId = -1
-        fun makeMessage(from: User?, chat: Chat, date: Date = Date(), type: String = "text", payload: Any?): BaseMessage {
+        fun makeMessage(from: User?, chat: Chat, date: Date = Date(), type: String = "text", payload: Any?, isIncoming: Boolean = false): BaseMessage {
             lastId++
             return when(type) {
-                "image" -> ImageMessage("$lastId", from, chat, date = date, image = payload as String)
-                else -> TextMessage("$lastId", from, chat, date = date, text = payload as String)
+                "image" -> ImageMessage("$lastId", from = from, chat = chat, date = date, image = payload as String, isIncoming = isIncoming)
+                else -> TextMessage("$lastId", from = from, chat = chat, date = date, text = payload as String, isIncoming = isIncoming)
             }
         }
     }
