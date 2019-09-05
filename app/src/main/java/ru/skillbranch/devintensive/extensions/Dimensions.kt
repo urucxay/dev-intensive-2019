c130724683c3a297083bad6e12c0ed678c3f8d1a
UrucXay <UrucXay@gmail.com>
2019-09-05 12:45:09 +0300

    Delete unused imports, refactor

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Dimensions.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Dimensions.kt
@@ -1,6 +1,5 @@
 package ru.skillbranch.devintensive.extensions
 
-import android.content.Context
 import android.content.res.Resources
 
 fun Int.toDp() : Int = (this / Resources.getSystem().displayMetrics.density + 0.5f).toInt()
--- app/src/main/java/ru/skillbranch/devintensive/repositories/ChatRepository.kt
+++ app/src/main/java/ru/skillbranch/devintensive/repositories/ChatRepository.kt
@@ -3,8 +3,6 @@ package ru.skillbranch.devintensive.repositories
 import androidx.lifecycle.MutableLiveData
 import ru.skillbranch.devintensive.data.managers.CacheManager
 import ru.skillbranch.devintensive.models.data.Chat
-import ru.skillbranch.devintensive.models.data.ChatItem
-import ru.skillbranch.devintensive.utils.DataGenerator
 
 object ChatRepository {
     private val chats = CacheManager.loadChats()
